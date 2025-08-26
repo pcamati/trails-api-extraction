c0238f0e16b794c1f59289bcdc51abaee72c2619
Patrice Camati <patrice.camati@gmail.com>
2025-08-26 10:20:46 -0300

    first request made

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Error;
+
+const BASE_URL: &str = "https://trailsinthedatabase.com";
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let client = reqwest::Client::new();
+    let resp = client
+        .get(BASE_URL)
+        .header("User-Agent", "my-rust-client")
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("{}", resp);
+    Ok(())
 }
